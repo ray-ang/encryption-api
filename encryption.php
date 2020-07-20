bcbbd727378742258b4a1398ebdb4ffa1e0672fc
ray-ang <46805160+ray-ang@users.noreply.github.com>
2020-07-19 22:05:27 -0400

    JSON.stringify( ) format in request body
    Allow processing of multiple data fields using Javascript JSON.stringify() format

--- encryption.php
+++ encryption.php
@@ -18,25 +18,27 @@ define('PASS_PHRASE', 'MySecret12345'); // Define PASS_PHRASE
 
 function encrypt($plaintext)
 {
-	function encrypt_v1($plaintext)
-	{
-		// Version, Cipher Method and Initialization Vector
-		$version = 'enc-v1';
-		$cipher = 'aes-256-ctr';
-		$iv = random_bytes(16);
+	if (! function_exists('encrypt_v1')) {
+		function encrypt_v1($plaintext)
+		{
+			// Version, Cipher Method and Initialization Vector
+			$version = 'enc-v1';
+			$cipher = 'aes-256-ctr';
+			$iv = random_bytes(16);
 
-		// Salt and Keys
-		$salt = random_bytes(16);
-		$key = hash_pbkdf2('sha256', PASS_PHRASE, $salt, 10000); // Encryption
-		$key_hmac = hash_pbkdf2('sha256', PASS_PHRASE, $salt, 10000); // HMAC
+			// Salt and Keys
+			$salt = random_bytes(16);
+			$key = hash_pbkdf2('sha256', PASS_PHRASE, $salt, 10000); // Encryption
+			$key_hmac = hash_pbkdf2('sha256', PASS_PHRASE, $salt, 10000); // HMAC
 
-		// Ciphertext and Hash
-		$ciphertext = openssl_encrypt($plaintext, $cipher, $key, $options=0, $iv);
-		$hash = hash_hmac('sha256', $ciphertext, $key_hmac);
+			// Ciphertext and Hash
+			$ciphertext = openssl_encrypt($plaintext, $cipher, $key, $options=0, $iv);
+			$hash = hash_hmac('sha256', $ciphertext, $key_hmac);
 
-		return $version . '::' . base64_encode($ciphertext) . '::' . base64_encode($hash) . '::' . base64_encode($iv) . '::' . base64_encode($salt);
+			return $version . '::' . base64_encode($ciphertext) . '::' . base64_encode($hash) . '::' . base64_encode($iv) . '::' . base64_encode($salt);
+		}
 	}
-
+	
 	/* Version-based Encryption */
 	// Default encryption function
 	return encrypt_v1($plaintext);
@@ -50,29 +52,31 @@ function encrypt($plaintext)
 
 function decrypt($encrypted)
 {
-	function decrypt_v1($encrypted)
-	{
-		// Cipher method
-		$cipher = 'aes-256-ctr';
+	if (! function_exists('decrypt_v1')) {
+		function decrypt_v1($encrypted)
+		{
+			// Cipher method
+			$cipher = 'aes-256-ctr';
 
-		list($version, $ciphertext, $hash, $iv, $salt) = explode('::', $encrypted);
-		$ciphertext = base64_decode($ciphertext);
-		$hash = base64_decode($hash);
-		$iv = base64_decode($iv);
-		$salt = base64_decode($salt);
+			list($version, $ciphertext, $hash, $iv, $salt) = explode('::', $encrypted);
+			$ciphertext = base64_decode($ciphertext);
+			$hash = base64_decode($hash);
+			$iv = base64_decode($iv);
+			$salt = base64_decode($salt);
 
-		// Derive Keys
-		$key = hash_pbkdf2('sha256', PASS_PHRASE, $salt, 10000); // Decryption
-		$key_hmac = hash_pbkdf2('sha256', PASS_PHRASE, $salt, 10000); // HMAC
+			// Derive Keys
+			$key = hash_pbkdf2('sha256', PASS_PHRASE, $salt, 10000); // Decryption
+			$key_hmac = hash_pbkdf2('sha256', PASS_PHRASE, $salt, 10000); // HMAC
 
-		// Calculate hash of ciphertext
-		$digest = hash_hmac('sha256', $ciphertext, $key_hmac);
+			// Calculate hash of ciphertext
+			$digest = hash_hmac('sha256', $ciphertext, $key_hmac);
 
-		// HMAC Authentication
-		if  ( hash_equals($hash, $digest) ) {
-			return openssl_decrypt($ciphertext, $cipher, $key, $options=0, $iv);
-		} else {
-			exit('Please verify authenticity of ciphertext.');
+			// HMAC Authentication
+			if  ( hash_equals($hash, $digest) ) {
+				return openssl_decrypt($ciphertext, $cipher, $key, $options=0, $iv);
+			} else {
+				exit('Please verify authenticity of ciphertext.');
+			}
 		}
 	}
 
@@ -98,11 +102,19 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 	exit();
 }
 
-$body = file_get_contents('php://input'); // Plaintext or encoded ciphertext
+$body = file_get_contents('php://input'); // Javascript JSON.stringify() format
 
 /* Require request body (not enctype="multipart/form-data") */
 if (empty($body)) {
 	header('HTTP/1.1 400 The request should have a body, and must not be enctype="multipart/form-data".');
+	exit();
+}
+
+/* Require request body to be in JSON format */
+$body_array = json_decode($body, TRUE); // Convert JSON body string into array
+
+if (! is_array($body_array)) {
+	header('HTTP/1.1 400 The request body should be in JSON format.');
 	exit();
 }
 
@@ -112,11 +124,16 @@ if (! isset($_GET['action']) || empty($_GET['action'])) {
 	exit();
 }
 
+$body_obj = json_decode($body);
 $action = $_GET['action'];
 
 /* Execute Function */
 if (function_exists($action)) {
-	echo $action($body);
+	$data = new stdClass();
+	foreach($body_obj as $key => $value) {
+		$data->$key = $action($value);
+	}
+	echo json_encode($data);
 } else {
 	header('HTTP/1.1 400 Please set "action" parameter to either "encrypt" or "decrypt".');
 	exit();
